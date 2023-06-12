0ae896440778958ee25d62752785f12f53674f7f
Riakol <40207895+Riakol@users.noreply.github.com>
2023-06-12 10:41:50 +0600

    Add files via upload

---
+++ 8kyu/How_good_are_you_really.py
@@ -0,0 +1,7 @@
+def better_than_average(class_points, your_points):
+   return your_points > sum(class_points)/len(class_points)
+
+
+'''
+https://www.codewars.com/kata/5601409514fc93442500010b
+'''
