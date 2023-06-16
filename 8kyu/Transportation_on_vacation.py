fca8a5c8ad09659aac26b0e6fe46045d69d1818b
Riakol <40207895+Riakol@users.noreply.github.com>
2023-06-16 12:02:13 +0600

    Add files via upload

---
+++ 8kyu/Transportation_on_vacation.py
@@ -0,0 +1,12 @@
+def rental_car_cost(d):
+    if d >= 7:
+        return (d*40)-50
+    elif 3 <= d < 7:
+        return (d*40)-20
+    else:
+        return d * 40
+
+
+'''
+https://www.codewars.com/kata/568d0dd208ee69389d000016
+'''
