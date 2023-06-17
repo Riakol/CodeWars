9973aae352860aed02fc7fa14a4ea2d3580faf8f
Riakol <40207895+Riakol@users.noreply.github.com>
2023-06-17 14:49:30 +0600

    Add files via upload

---
+++ 6kyu/Create_Phone_Number.py
@@ -0,0 +1,9 @@
+def create_phone_number(n):
+    a, b, c, d, e, f, z, x, m, v = n
+    return f'({a}{b}{c}) {d}{e}{f}-{z}{x}{m}{v}'
+                       
+
+
+'''
+https://www.codewars.com/kata/525f50e3b73515a6db000b83
+'''
